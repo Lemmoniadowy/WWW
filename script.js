4988375a699b36d8f23db491598ff4cdc32d2d7a
Daerato <lemmoniadowy@gmail.com>
2020-06-02 23:37:14 +0200

    push all

---
+++ script.js
@@ -0,0 +1,7 @@
+$("button").click(function() {
+    $(".zmien").css({"display":"block"});
+ $(".pokaz").css({"display":"none"});
+    $("button").click(function() {
+    $(".zmien").css({"display":"none"});
+ $(".pokaz").css({"display":"block"});
+});});
